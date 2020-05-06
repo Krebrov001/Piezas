3dbd684e6357f5cb783441a1c294967abaeb319f
Krebrov001 <krebrov001@student.butte.edu>
2020-05-05 22:58:00 -0700

    I added the following features:
     - Piezas::Piexas()
     - Piezas::reset()
     - Piezas::dropPiece()
     - Piezas::pieceAt()
     - test_constructor

--- Piezas.cpp
+++ Piezas.cpp
@@ -1,8 +1,9 @@
 #include "Piezas.h"
 #include <vector>
+
 /** CLASS Piezas
  * Class for representing a Piezas vertical board, which is roughly based
- * on the game "Connect Four" where pieces are placed in a column and 
+ * on the game "Connect Four" where pieces are placed in a column and
  * fall to the bottom of the column, or on top of other pieces already in
  * that column. For an illustration of the board, see:
  *  https://en.wikipedia.org/wiki/Connect_Four
@@ -15,13 +16,30 @@
  * dropped in column 2 should take [1,2].
 **/
 
+static const int num_rows = 3;
+static const int num_cols = 4;
 
 /**
- * Constructor sets an empty board (default 3 rows, 4 columns) and 
+ * Constructor sets an empty board (default 3 rows, 4 columns) and
  * specifies it is X's turn first
 **/
 Piezas::Piezas()
 {
+    // set default 3 rows, 4 columns
+    board.resize(num_rows);
+    for (int row = 0; row < num_rows; ++row) {
+        board[row].resize(num_cols);
+    }
+
+    // set an empty board
+    for (int row = 0; row < num_rows; ++row) {
+        for (int col = 0; col < num_cols; ++col) {
+            board[row][col] = Blank;
+        }
+    }
+
+    // specify it is X's turn first
+    turn = X;
 }
 
 /**
@@ -30,19 +48,63 @@ Piezas::Piezas()
 **/
 void Piezas::reset()
 {
+    // set an empty board
+    for (int row = 0; row < num_rows; ++row) {
+        for (int col = 0; col < num_cols; ++col) {
+            board[row][col] = Blank;
+        }
+    }
 }
 
 /**
  * Places a piece of the current turn on the board, returns what
- * piece is placed, and toggles which Piece's turn it is. dropPiece does 
+ * piece is placed, and toggles which Piece's turn it is. dropPiece does
  * NOT allow to place a piece in a location where a column is full.
- * In that case, placePiece returns Piece Blank value 
+ * In that case, placePiece returns Piece Blank value
  * Out of bounds coordinates return the Piece Invalid value
  * Trying to drop a piece where it cannot be placed loses the player's turn
-**/ 
+**/
 Piece Piezas::dropPiece(int column)
 {
-    return Blank;
+    Piece piece = Blank;
+
+    // Out of bounds coordinates
+    if (column < 0 || column >= num_cols) {
+        piece = Invalid;
+    // Inside bounds coordinates
+    } else {
+        bool found_location = false;
+        int row = 2;
+        // Because of the flipped orientation, when you drop the pieces,
+        // they rise up to the top.
+        for (row = 2; row >= 0; --row) {
+            // If current space is occupied
+            if (board[row][column] != Blank) {
+                found_location = false;
+                break;
+            // If previous space is occupied && current space is free
+            } else if (board[row - 1][column] != Blank && board[row][column] == Blank) {
+                found_location = true;
+                break;
+            // If row == 0 && current space is free
+            } else if (row == 0 && board[row][column] == Blank) {
+                found_location = true;
+                break;
+            }
+        }
+
+        if (found_location) {
+            board[row][column] = turn;
+            piece = board[row][column];
+        } else {
+            piece = Blank;
+        }
+    }
+
+    // set the turn to the other player
+    turn = (turn == X) ? O : X;
+
+    return piece;
 }
 
 /**
@@ -51,13 +113,22 @@ Piece Piezas::dropPiece(int column)
 **/
 Piece Piezas::pieceAt(int row, int column)
 {
-    return Blank;
+    const bool row_invalid = row < 0 || row >= num_rows;
+    const bool col_invalid = column < 0 || column >= num_cols;
+
+    if (row_invalid || col_invalid) {
+        return Invalid;
+    } else {
+        // This can return either the piece or a Blank.
+        // By default when the board is cleared, all values are set to Blank.
+        return board[row][column];
+    }
 }
 
 /**
  * Returns which Piece has won, if there is a winner, Invalid if the game
  * is not over, or Blank if the board is filled and no one has won ("tie").
- * For a game to be over, all locations on the board must be filled with X's 
+ * For a game to be over, all locations on the board must be filled with X's
  * and O's (i.e. no remaining Blank spaces). The winner is which player has
  * the most adjacent pieces in a single line. Lines can go either vertically
  * or horizontally. If both X's and O's have the same max number of pieces in a
--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -4,17 +4,42 @@
 
 #include <gtest/gtest.h>
 #include "Piezas.h"
- 
+
 class PiezasTest : public ::testing::Test
 {
 	protected:
 		PiezasTest(){} //constructor runs before each test
 		virtual ~PiezasTest(){} //destructor cleans up after tests
 		virtual void SetUp(){} //sets up before each test (after constructor)
-		virtual void TearDown(){} //clean up after each test, (before destructor) 
+		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
+
+#define const const
 
 TEST(PiezasTest, sanityCheck)
 {
 	ASSERT_TRUE(true);
+}
+
+
+TEST(PiezasTest, test_constructor)
+{
+    const int num_rows = 3;
+    const int num_cols = 4;
+
+    Piezas game;
+
+    // The constructor sets an empty board, which means that all the spaces have to be blank.
+    // They cannot contain any garbage values.
+    // This for loop is only one of the few places where we need to violate the one assert rule.
+    for (int row = 0; row < num_rows; ++row) {
+        for (int col = 0; col < num_cols; ++col) {
+            Piece piece = game.pieceAt(row, col);
+            ASSERT_EQ(piece, Blank);
+        }
+    }
+
+    // When I drop the piece, it should be piece X.
+    Piece piece = game.dropPiece(0);
+    ASSERT_EQ(piece, X);
 }
